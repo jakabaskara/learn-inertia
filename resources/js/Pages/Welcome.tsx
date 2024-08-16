d535280b9447090c3e84b9e12a4ec8f06c4c34ec
jakabaskara <jaka.baskara30@gmail.com>
2024-08-16 22:52:21 +0700

    first commit

---
+++ resources/js/Pages/Welcome.tsx
@@ -0,0 +1,15 @@
+
+export default function Welcome() {
+    const handleImageError = () => {
+        document.getElementById('screenshot-container')?.classList.add('!hidden');
+        document.getElementById('docs-card')?.classList.add('!row-span-1');
+        document.getElementById('docs-card-content')?.classList.add('!flex-row');
+        document.getElementById('background')?.classList.add('!hidden');
+    };
+
+    return (
+        <div>
+            Welcome Inertia
+        </div>
+    );
+}
